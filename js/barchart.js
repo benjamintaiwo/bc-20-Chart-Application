c996d9845410f5499654bb31ff3f114f61240f2f
benjamintaiwo <abegundetaiwo@gmail.com>
2017-04-20 09:08:46 +0100

    Add Js file for calling each chart script

---
+++ js/barchart.js
@@ -0,0 +1,20 @@
+//function to plot bar chat			  
+function barchart(dataset, max) {
+	var canvas = document.getElementById('chart');
+	var context = canvas.getContext('2d');
+	var startx = 80;  
+	var scale_f = (canvas.height-200)/max;				        
+	context.clearRect(0, 0, canvas.width, canvas.height);
+    plotaxis();
+                
+	for(var s in dataset){			    
+		context.beginPath();
+        context.rect(startx,600, 30,-dataset[s].percent*scale_f); //multiply by 10 to improve scaling
+		context.fillStyle = '#B71C1C';
+		context.fill();
+		context.font = '10pt Helvetica';
+		context.fillStyle = 'black';
+    	context.fillText(dataset[s].name, startx, 620);
+		startx += 50;
+	}			        
+}
