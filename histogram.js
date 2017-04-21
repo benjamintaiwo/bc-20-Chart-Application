a05d8c05da0f10cf6c058e6375616207dce512d8
benjamintaiwo <abegundetaiwo@gmail.com>
2017-04-21 09:49:50 +0100

    Edit startx: x axis position

--- histogram.js
+++ histogram.js
@@ -2,7 +2,7 @@
 function histogram(dataset, max) {
 	var canvas = document.getElementById('chart');
 	var context = canvas.getContext('2d');
-	var startx = 81; 
+	var startx = 51; 
 	var chartcolors = ['#B71C1C','#4A148C', '#1A237E', '#01579B', '#004D40', 
 						'#33691E', '#F57F17', '#E65100', '#3E2723', '#263238']; 
 	var scale_f = (canvas.height-200)/max; //calculate scale factor based on maximum user input				        
