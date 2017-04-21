99cf42939785568fb1706c06fa90561393d20075
benjamintaiwo <abegundetaiwo@gmail.com>
2017-04-21 08:53:50 +0100

    add scaling and call plotaxis function

--- histogram.js
+++ histogram.js
@@ -5,5 +5,9 @@ function histogram(dataset, max) {
 	var startx = 51; 
 	var chartcolors = ['#B71C1C','#4A148C', '#1A237E', '#01579B', '#004D40', 
 						'#33691E', '#F57F17', '#E65100', '#3E2723', '#263238']; 
-	       
+	var scale_f = (canvas.height-200)/max; //calculate scale factor based on maximum user input				        
+	context.clearRect(0, 0, canvas.width, canvas.height);
+    plotaxis();
+                
+		        
 }
