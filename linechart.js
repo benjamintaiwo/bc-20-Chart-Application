557a7cc01fe4b37aa29a7716f2d08897a0ed61e2
benjamintaiwo <abegundetaiwo@gmail.com>
2017-04-21 12:09:09 +0100

    add dataset plotting

--- linechart.js
+++ linechart.js
@@ -8,6 +8,19 @@ function linechart(dataset, max) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	plotaxis();
 	context.beginPath();
+	
+		//convert percentage in radians and plot graph			       
+	for(var s in dataset){	
+		//create line   
+		context.lineTo(startx, (600-dataset[s].percent*scale_f));//y=axis numbers multiplied by 5 to improve scaling
+		context.lineWidth = 2;
+		context.strokeStyle = '#F44336';
+		context.stroke();
+		//create Labels
+		context.font = '10pt Helvetica';
+		context.fillStyle = 'black';
+    	context.fillText(dataset[s].name, startx, 620);
+		startx += 50;
 		
 
 	}	
