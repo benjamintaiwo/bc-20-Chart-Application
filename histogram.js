0f4eca659628ba3604e14687ad00f557a0a6e134
benjamintaiwo <abegundetaiwo@gmail.com>
2017-04-21 08:35:21 +0100

    add histogram function- getting the context

---
+++ histogram.js
@@ -0,0 +1,9 @@
+//function to plot histogram
+function histogram(dataset, max) {
+	var canvas = document.getElementById('chart');
+	var context = canvas.getContext('2d');
+	var startx = 51; 
+	var chartcolors = ['#B71C1C','#4A148C', '#1A237E', '#01579B', '#004D40', 
+						'#33691E', '#F57F17', '#E65100', '#3E2723', '#263238']; 
+	       
+}
